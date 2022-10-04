39b13adce0c2a11eec6e4501df3959af3825ac58
Luke <lukasmtorquato@gmail.com>
2022-10-04 03:56:36 -0300

    All done for the day

--- VolumeMixer/App.xaml.cs
+++ VolumeMixer/App.xaml.cs
@@ -13,5 +13,9 @@ namespace VolumeMixer
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzMwNzU4QDMyMzAyZTMzMmUzMG01WDI4NGNpMGhPZnBKeGZZR3ErQU0xNFE4cVFFVERYTTBsUU1Gc1NxQUk9");
+        }
     }
 }
